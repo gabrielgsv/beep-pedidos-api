5d0ecccfe0a58b9f93d65fd7cf585a8aa6008755
gabrielgsv <gabriel.soares.vilela@gmail.com>
2023-11-07 14:31:43 -0300

    added user and auth with token

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
+  };
+
+  app.enableCors(options);
+
+  const config = new DocumentBuilder()
+    .setTitle('Beep Pedidos API')
+    .setDescription('A REST API for Beep Pedidos')
+    .setVersion('1.0')
+    .addTag('beep-pedidos')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
+
+  app.setGlobalPrefix('v1');
+
   await app.listen(3000);
 }
 bootstrap();
