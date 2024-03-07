f13aa419dce7e5e4154d71ab10fc34f9dd4cda33
gabrielgsv <gabriel.soares.vilela@gmail.com>
2024-03-06 21:18:30 -0300

    fixed subscription function

--- src/payment/payment.service.ts
+++ src/payment/payment.service.ts
@@ -13,19 +13,22 @@ export class PaymentService {
 
   async createSession() {
     const session = await this.stripe.checkout.sessions.create({
-      ui_mode: 'embedded',
+      mode: 'subscription',
       line_items: [
         {
-          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
           price: process.env.STRIPE_PRICE_ID,
+          // For metered billing, do not pass quantity
           quantity: 1,
         },
       ],
-      mode: 'payment',
-      return_url: `https://www.dicio.com.br/sucesso?success=true`,
+      // {CHECKOUT_SESSION_ID} is a string literal; do not change it!
+      // the actual Session ID is returned in the query parameter when your customer
+      // is redirected to the success page.
+      success_url: `${process.env.FRONT_URL}/dashboard/payment/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.FRONT_URL}/dashboard/payment/canceled`,
     });
 
-    return session.client_secret;
+    return session;
   }
 
   async getSessionStatus(sessionId: string) {
